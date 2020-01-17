910719a56d423a41202bfa8ec209e7b103e5fd5e
Roman Maik <ramanmaik0@gmail.com>
2020-01-17 22:27:54 +0200

    lesson19

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './comppnents/Header/Header';
 import Navbar from './comppnents/Navbar/Navbar';
-import Profile from './comppnents/Profile/Profile'
+import Profile from './comppnents/Profile/Profile';
+import News from "./comppnents/News/News";
+import Music from "./comppnents/Music/Music";
+import Settings from "./comppnents/Settings/Settings";
 import Messages from "./comppnents/Messages/Messages";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app_wrapper">
-      <Header />
-      <Navbar />
-      <div className="app_wrapper_content">
-          <Messages/>
-      </div>
-      {/*<Profile />*/}
-    </div>
+      <BrowserRouter>
+          <div className="app_wrapper">
+              <Header />
+              <Navbar />
+              <div className="app_wrapper_content">
+                  <Route path="/messages" component={Messages}/>
+                  <Route path="/profile" component={Profile}/>
+                  <Route path="/news" component={News}/>
+                  <Route path="/music" component={Music}/>
+                  <Route path="/settings" component={Settings}/>
+              </div>
+          </div>
+      </BrowserRouter>
   );
 }
 
