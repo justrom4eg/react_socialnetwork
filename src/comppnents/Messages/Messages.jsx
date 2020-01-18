607384e53a63992942c5a54999959ec5662c45b9
Roman Maik <ramanmaik0@gmail.com>
2020-01-18 09:34:32 +0200

    dialogs

--- src/comppnents/Messages/Messages.jsx
+++ src/comppnents/Messages/Messages.jsx
@@ -3,8 +3,28 @@ import classes from "./Messages.module.css"
 
 const Messages = () => {
     return (
-        <div>
-            Messages
+        <div className={classes.dialogs}>
+
+            <div className={classes.dialogs_item}>
+                <div className={classes.dialog}>
+                    Dima
+                </div>
+                <div className={classes.dialog}>
+                    Nazar
+                </div>
+                <div className={classes.dialog}>
+                    Ira
+                </div>
+                <div className={classes.dialog}>
+                    Romu4
+                </div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Yo how are you?</div>
+                <div className={classes.message}>How is your it-kamasutra?</div>
+                <div className={classes.message}>I like it</div>
+            </div>
         </div>
     )
 }
